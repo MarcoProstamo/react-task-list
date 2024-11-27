c3b8e5ca24748d01317c7c436666c037714f2b5e
MarcoProstamo <Marcopros1201@gmail.com>
2024-11-27 14:51:56 +0100

    Add → Output Lists

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,57 @@ import { tasks } from "./data/tasksData";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const completedTasks = tasks.filter((task) => task.state === "completed");
+  const inProgressTasks = tasks.filter((task) => task.state !== "completed");
+
+  function completedTasksList() {
+    return (
+      <>
+        <h2>Lista Attività Completate</h2>
+        <ul>
+          {completedTasks.map((task, index) => (
+            <li key={index}>
+              <span>
+                <strong>Titolo:</strong> {task.title}
+                <br />
+                <strong>Priorità:</strong> {task.priority}
+                <br />
+                <strong>Tempo Stimato:</strong> {task.estimatedTime}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+
+  function inProgressTasksList() {
+    return (
+      <>
+        <h2>Lista Attività Da Completare</h2>
+        <ul>
+          {inProgressTasks.map((task, index) => (
+            <li key={index}>
+              <span>
+                <strong>Titolo:</strong> {task.title}
+                <br />
+                <strong>Priorità:</strong> {task.priority}
+                <br />
+                <strong>Tempo Stimato:</strong> {task.estimatedTime}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+
+  return (
+    <>
+      {inProgressTasksList()}
+      {completedTasksList()}
+    </>
+  );
 }
 
 export default App;
