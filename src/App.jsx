d01a6dd0db1a831488d8e2b45d6fe8828083f67b
MarcoProstamo <Marcopros1201@gmail.com>
2024-11-27 15:21:43 +0100

    Add → Graphic Output

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,73 @@
 import { useState } from "react";
 import { tasks } from "./data/tasksData";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   const completedTasks = tasks.filter((task) => task.state === "completed");
   const inProgressTasks = tasks.filter((task) => task.state !== "completed");
 
-  function completedTasksList() {
+  function generateList(list) {
     return (
-      <>
-        <h2>Lista Attività Completate</h2>
-        <ul>
-          {completedTasks.map((task, index) => (
-            <li key={index}>
-              <span>
-                <strong>Titolo:</strong> {task.title}
-                <br />
-                <strong>Priorità:</strong> {task.priority}
-                <br />
-                <strong>Tempo Stimato:</strong> {task.estimatedTime}
-              </span>
-            </li>
-          ))}
-        </ul>
-      </>
+      <ul className="list-unstyled">
+        {list.map((task, index) => (
+          <li key={index}>
+            <span className="fw-semibold">
+              <span className="fw-bold">{task.title}</span>
+              {task.state === "completed" ? (
+                <span class="badge rounded-pill text-bg-success text-capitalize mx-2">
+                  {task.state}
+                </span>
+              ) : (
+                <span class="badge rounded-pill text-bg-warning text-capitalize mx-2">
+                  {task.state}
+                </span>
+              )}
+              <br />
+              Priority: {task.priority}
+              <br />
+              Est. Time: {task.estimatedTime}
+            </span>
+          </li>
+        ))}
+      </ul>
     );
   }
 
   function inProgressTasksList() {
     return (
       <>
-        <h2>Lista Attività Da Completare</h2>
-        <ul>
-          {inProgressTasks.map((task, index) => (
-            <li key={index}>
-              <span>
-                <strong>Titolo:</strong> {task.title}
-                <br />
-                <strong>Priorità:</strong> {task.priority}
-                <br />
-                <strong>Tempo Stimato:</strong> {task.estimatedTime}
-              </span>
-            </li>
-          ))}
-        </ul>
+        <h2 className="h5 fw-semibold my-3">
+          Current Tasks ({inProgressTasks.length})
+        </h2>
+        {generateList(inProgressTasks)}
+      </>
+    );
+  }
+
+  function completedTasksList() {
+    return (
+      <>
+        <h2 className="h5 fw-semibold my-3">
+          Completed Tasks ({completedTasks.length})
+        </h2>
+        {generateList(completedTasks)}
       </>
     );
   }
 
   return (
     <>
-      {inProgressTasksList()}
-      {completedTasksList()}
+      <div>
+        <div className="bg-primary py-3 px-3">
+          <h1 className="fw-semibold">Task Manager</h1>
+        </div>
+        <div className="px-3">
+          {inProgressTasksList()}
+          <hr class="border border-secondary border opacity-50" />
+          {completedTasksList()}
+        </div>
+      </div>
     </>
   );
 }
